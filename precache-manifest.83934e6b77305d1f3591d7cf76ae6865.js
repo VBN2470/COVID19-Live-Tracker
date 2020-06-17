5a6f1535d97b58cd74fec91f80180ddb5ee53e47
Vishaal Nathan <vbn2470@gmail.com>
2020-06-18 00:36:28 +1000

    Updates

---
+++ precache-manifest.83934e6b77305d1f3591d7cf76ae6865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7db1114e962aec26a373174b57821b4",
+    "url": "/VBN2470/COVID19-Live-Tracker/index.html"
+  },
+  {
+    "revision": "66fb10cf0c3fedb77416",
+    "url": "/VBN2470/COVID19-Live-Tracker/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d01b62cea23804f24225",
+    "url": "/VBN2470/COVID19-Live-Tracker/static/css/main.dde22dd5.chunk.css"
+  },
+  {
+    "revision": "66fb10cf0c3fedb77416",
+    "url": "/VBN2470/COVID19-Live-Tracker/static/js/2.6857e99d.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/VBN2470/COVID19-Live-Tracker/static/js/2.6857e99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01b62cea23804f24225",
+    "url": "/VBN2470/COVID19-Live-Tracker/static/js/main.80ffbde6.chunk.js"
+  },
+  {
+    "revision": "986a80d6c82bcecf68b1",
+    "url": "/VBN2470/COVID19-Live-Tracker/static/js/runtime-main.8bd101cf.js"
+  }
+]);
